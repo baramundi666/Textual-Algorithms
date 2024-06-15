b56e2e3e735084a5f86e669c21485866040a3d4e
Mateusz Król <mateuszkrol221@gmail.com>
2024-06-15 22:40:28 +0200

    lab3 upload

--- 3/matching_test.go
+++ 3/matching_test.go
@@ -1,6 +1,7 @@
 package matching
 
 import (
+	"os"
 	"slices"
 	"testing"
 )
@@ -32,4 +33,208 @@ func indices(pat, text []byte) []int {
 		}
 	}
 	return r
+}
+
+func TestNaive(t *testing.T) {
+	pat := []byte("chmur")
+	text := []byte("Pochmurne chmury")
+
+	got := []int{}
+	Naive(pat, text, func(n int) { got = append(got, n) })
+	want := indices(pat, text)
+	if !slices.Equal(got, want) {
+		t.Errorf("error!")
+	}
+}
+
+func TestBackwardNaive(t *testing.T) {
+	pat := []byte("chmur")
+	text := []byte("Pochmurne chmury")
+
+	got := []int{}
+	BackwardNaive(pat, text, func(n int) { got = append(got, n) })
+	want := indices(pat, text)
+	if !slices.Equal(got, want) {
+		t.Errorf("error!")
+	}
+}
+
+func TestBoyerMoore(t *testing.T) {
+	pat := []byte("chmur")
+	text := []byte("Pochmurne chmury")
+
+	got := []int{}
+	BoyerMoore(pat, text, func(n int) { got = append(got, n) })
+	want := indices(pat, text)
+	if !slices.Equal(got, want) {
+		t.Errorf("error!")
+	}
+}
+
+func TestKMP(t *testing.T) {
+	pat := []byte("chmur")
+	text := []byte("Pochmurne chmury")
+
+	got := []int{}
+	KMP(pat, text, func(n int) { got = append(got, n) })
+	want := indices(pat, text)
+	if !slices.Equal(got, want) {
+		t.Errorf("error!")
+	}
+}
+
+func TestKarpRabin(t *testing.T) {
+	pat := []byte("chmur")
+	text := []byte("Pochmurne chmury")
+
+	got := []int{}
+	KarpRabin(pat, text, func(n int) { got = append(got, n) })
+	want := indices(pat, text)
+	if !slices.Equal(got, want) {
+		t.Errorf("error!")
+	}
+}
+
+func TestShiftOr(t *testing.T) {
+	pat := []byte("chmur")
+	text := []byte("Pochmurne chmury")
+
+	got := []int{}
+	ShiftOr(pat, text, func(n int) { got = append(got, n) })
+	want := indices(pat, text)
+	if !slices.Equal(got, want) {
+		t.Errorf("error!")
+	}
+}
+
+func BenchmarkShortNaive(b *testing.B) {
+	pat := []byte("chmura")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Naive(pat, text, func(int) {})
+	}
+}
+
+func BenchmarkShortBackwardNaive(b *testing.B) {
+	pat := []byte("chmur")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		BackwardNaive(pat, text, func(int) {})
+	}
+}
+
+func BenchmarkShortBoyerMoore(b *testing.B) {
+	pat := []byte("chmur")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		BoyerMoore(pat, text, func(int) {})
+	}
+}
+
+func BenchmarkShortKMP(b *testing.B) {
+	pat := []byte("chmur")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		KMP(pat, text, func(int) {})
+	}
+}
+
+// func BenchmarkShortKarpRabin(b *testing.B) {
+// 	pat := []byte("chmur")
+// 	text, _ := os.ReadFile("kordian.txt")
+
+// 	b.ResetTimer()
+
+// 	for i := 0; i < b.N; i++ {
+// 		KarpRabin(pat, text, func(int) {})
+// 	}
+// }
+
+func BenchmarkShortShiftOr(b *testing.B) {
+	pat := []byte("chmur")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ShiftOr(pat, text, func(int) {})
+	}
+}
+
+func BenchmarkLongNaive(b *testing.B) {
+	pat := []byte("katedralnego")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Naive(pat, text, func(int) {})
+	}
+}
+
+func BenchmarkLongBackwardNaive(b *testing.B) {
+	pat := []byte("katedralnego")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		BackwardNaive(pat, text, func(int) {})
+	}
+}
+
+func BenchmarkLongBoyerMoore(b *testing.B) {
+	pat := []byte("katedralnego")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		BoyerMoore(pat, text, func(int) {})
+	}
+}
+
+func BenchmarkLongKMP(b *testing.B) {
+	pat := []byte("katedralnego")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		KMP(pat, text, func(int) {})
+	}
+}
+
+// func BenchmarkLongKarpRabin(b *testing.B) {
+// 	pat := []byte("katedralnego")
+// 	text, _ := os.ReadFile("kordian.txt")
+
+// 	b.ResetTimer()
+
+// 	for i := 0; i < b.N; i++ {
+// 		KarpRabin(pat, text, func(int) {})
+// 	}
+// }
+
+func BenchmarkLongShiftOr(b *testing.B) {
+	pat := []byte("katedralnego")
+	text, _ := os.ReadFile("kordian.txt")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ShiftOr(pat, text, func(int) {})
+	}
 }
