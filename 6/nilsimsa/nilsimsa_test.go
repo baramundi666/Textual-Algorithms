913fcf9624b50333c8b44544c9759267915deb0b
Mateusz Król <mateuszkrol221@gmail.com>
2024-06-15 22:40:52 +0200

    lab6 upload

--- 6/nilsimsa/nilsimsa_test.go
+++ 6/nilsimsa/nilsimsa_test.go
@@ -1,6 +1,8 @@
 package nilsimsa
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
@@ -8,7 +10,7 @@ func TestNilsimsa(t *testing.T) {
 	s1 := "To niedźwiedź czy może dźwiedź? Chyba nie dźwiedź."
 	s2 := "Czy to dźwiedź, czy niedźwiedź? Może nie dźwiedź."
 	s3 := "Najgłupsze zwierzę w dżungli? Niedźwiedź polarny."
-	data := []struct{
+	data := []struct {
 		s1, s2 string
 		want   int
 	}{
@@ -22,4 +24,26 @@ func TestNilsimsa(t *testing.T) {
 				d.s1, d.s2, got, d.want)
 		}
 	}
+}
+
+func TestNilsimsa2(t *testing.T) {
+	s1, err := os.ReadFile("W1.txt")
+	s2, err := os.ReadFile("W2.txt")
+	s3, err := os.ReadFile("W3.txt")
+	s4, err := os.ReadFile("W4.txt")
+	s5, err := os.ReadFile("W5.txt")
+	s6, err := os.ReadFile("W6.txt")
+	if err != nil {
+		t.Fatalf("Failed to read file: %v", err)
+	}
+
+	got := []int{}
+
+	got = append(got, HammingDistance(Nilsimsa(string(s1)), Nilsimsa(string(s2))))
+	got = append(got, HammingDistance(Nilsimsa(string(s1)), Nilsimsa(string(s3))))
+	got = append(got, HammingDistance(Nilsimsa(string(s1)), Nilsimsa(string(s4))))
+	got = append(got, HammingDistance(Nilsimsa(string(s1)), Nilsimsa(string(s5))))
+	got = append(got, HammingDistance(Nilsimsa(string(s1)), Nilsimsa(string(s6))))
+
+	fmt.Printf("Results: %v\n", got)
 }
